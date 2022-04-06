558857138e5a23ed8087b94b60a26643a362e188
chandelima <49618629+chandelima@users.noreply.github.com>
2022-04-06 09:27:40 -0300

    Add Tag Model

---
+++ Migrations/20220406122215_AddTag.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_first_crud.Migrations
+{
+    public partial class AddTag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TagId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Tag",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_TagId",
+                table: "Products",
+                column: "TagId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Tag_TagId",
+                table: "Products",
+                column: "TagId",
+                principalTable: "Tag",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Tag_TagId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Tag");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_TagId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "TagId",
+                table: "Products");
+        }
+    }
+}
--- Models/ProductModel.cs
+++ Models/ProductModel.cs
@@ -5,5 +5,7 @@ public class Product {
     public string Code { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public int CategoryId { get; set; }
     public Category MyProperty { get; set; }
+    public Tag Tag { get; set; }
 }
